07c0a1623450eba1ea811ad18abed2ae5610313e
joshuarule <joshua@joshuarule.com>
2014-02-12 19:20:31 -0500

    edits

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
 
   // sets correct answer
   // 0 = first option
-  q1a = 0;
-  q2a = 1;
-  q3a = 2;
-  q4a = 3;
-  q5a = 0;
+  q0a = 0;
+  q1a = 1;
+  q2a = 2;
+  q3a = 3;
+  q4a = 0;
 
   question = 0;
   score = 0;
@@ -18,15 +18,24 @@ $(document).ready(function() {
   var correct =   "<div class='result correct'>Correct!</div>";
   var incorrect = "<div class='result incorrect'>Incorrect!</div>";
 
+  // make submit button green when option selected
+
+  $('.option').click(function(){
+    $('button').addClass('active');
+  })
+
+  // find current question
+
+  function currentQuestion() {
+    return ".question:eq("+question+")";
+  }
+
   function next() {
     $(currentQuestion()).toggleClass('js-show');
     question +=1;
     $(currentQuestion()).toggleClass('js-show');
     console.log("question#: " + question);
-  }
-
-  function currentQuestion() {
-    return ".question:eq("+question+")";
+    $('button').removeClass('active');
   }
 
   function updateScore() {
@@ -35,56 +44,63 @@ $(document).ready(function() {
   }
 
   function correctAnswer() {
+    console.log('correct answer');
     score +=1;
     $(correct).prependTo($('.options')).hide().fadeIn(300);
     $('.correct').delay(1000).fadeOut(300, function(){
-      $(this).remove();
-      next();
-      updateScore();
+      if ($(".correct:animated").length === 0) {
+        $('.correct').remove();
+        next();
+        updateScore();
+      };
     });
   }
 
+
   function incorrectAnswer() {
+    console.log('incorrect answer');
     $(incorrect).prependTo($('.options')).hide().fadeIn(300);
-    $('.inccorrect').delay(1000).fadeOut(300, function(){
-      $(this).remove();
-      next();
-      updateScore();
+    $('.incorrect').delay(1000).fadeOut(300, function() {
+      if ($(".incorrect:animated").length === 0) {
+        $('.incorrect').remove();
+        next();
+        updateScore();
+      };
     });
   }
 
   function checkAnswer() {
     if($(currentQuestion() + ' input').is(':checked')) {
-      if (question == 0) {
-        if ($(".question:eq(0) .option:eq("+q1a+") input").is(':checked')){
+      if (question === 0) {
+        if ($(".question:eq(0) .option:eq("+q0a+") input").is(':checked')){
           correctAnswer();
         } else {
           incorrectAnswer();
         }
       } 
-      else if (question == 1) {
-        if ($(".question:eq(1) .option:eq("+q2a+") input").is(':checked')){
+      else if (question === 1) {
+        if ($(".question:eq(1) .option:eq("+q1a+") input").is(':checked')){
           correctAnswer();
         } else {
           incorrectAnswer();
         }
       } 
-      else if (question == 2) {
-        if ($(".question:eq(2) .option:eq("+q3a+") input").is(':checked')){
+      else if (question === 2) {
+        if ($(".question:eq(2) .option:eq("+q2a+") input").is(':checked')){
           correctAnswer();
         } else {
           incorrectAnswer();
         }
       } 
-      else if (question == 3) {
-        if ($(".question:eq(3) .option:eq("+q4a+") input").is(':checked')){
+      else if (question === 3) {
+        if ($(".question:eq(3) .option:eq("+q3a+") input").is(':checked')){
           correctAnswer();
         } else {
           incorrectAnswer();
         }
       } 
-      else if (question == 4) {
-        if ($(".question:eq(4) .option:eq("+q5a+") input").is(':checked')){
+      else if (question === 4) {
+        if ($(".question:eq(4) .option:eq("+q4a+") input").is(':checked')){
           correctAnswer();
         } else {
           incorrectAnswer();
